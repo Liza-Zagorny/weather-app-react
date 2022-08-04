095538886466670424cc74d737d362547c9a6ae8
Liza Neiman <5gb.place.for.love@gmail.com>
2022-08-04 13:24:36 +0300

    fixing bug with no changing temp after submit

--- src/components/CurrentTemp.js
+++ src/components/CurrentTemp.js
@@ -4,16 +4,20 @@ import "../css/CurrentTemp.css";
 function CurrentTemp(props) {
   const [tempValue, setTempValue] = useState(props.celsius);
   const [unit, setUnit] = useState("celsius");
+
+  if (props.celsius !== tempValue && unit === "celsius") {
+    setTempValue(props.celsius);
+  }
   function showFahrenheit(event) {
     event.preventDefault();
     setUnit("fahrenheit");
-    setTempValue(Math.round((props.celsius * 9) / 5 + 32));
+    setTempValue(Math.round((tempValue * 9) / 5 + 32));
   }
 
   function showCelsius(event) {
     event.preventDefault();
     setUnit("celsius");
-    setTempValue(props.celsius);
+    setTempValue(tempValue);
   }
 
   if (unit === "celsius")
